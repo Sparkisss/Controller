e9621c7f5a17dd3eb464e1df21d99c1629fef89d
Igor Siamykin <sparkisisss@gmail.com>
2024-09-07 20:11:17 +0300

    style: add archive page style

--- client/src/pages/ArchivePage.tsx
+++ client/src/pages/ArchivePage.tsx
@@ -1,15 +1,47 @@
-const ArchivePage = () => {
+import { Avatar, List } from 'antd';
+import { FC } from 'react';
 
-    const countTotal = (num: number) => {
-        console.log('...');
-        return num + 10;
-    }
+const ArchivePage: FC = () => {
+    const data = [
+        {
+            number: 1,
+            status: 'OK',
+            date: '07/09/2024',
+            time: '19:10',
+        },
+        {
+            number: 2,
+            status: 'OK',
+            date: '08/09/2024',
+            time: '19:55',
+        },
+        {
+            number: 3,
+            status: 'Attention',
+            date: '09/09/2024',
+            time: '11:10',
+        },
+        {
+            number: 4,
+            status: 'Error',
+            date: '17/09/2024',
+            time: '09:10',
+        },
+    ];
 
-    const total = countTotal(10);
     return (
-        <div>
-            archive page {total}
-        </div>
+        <List
+            pagination={{align: 'end'}}
+            itemLayout="horizontal"
+            dataSource={data}
+            renderItem={(item) => (
+                <List.Item>
+                    <List.Item.Meta                       
+                        title={item.number + ' ' + item.status + ' ' +  item.date + ' ' + item.time}
+                    />
+                </List.Item>
+            )}
+        />
     );
 };
 
